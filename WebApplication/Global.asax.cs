9fcf530fa9247fc3b5da4f951e8c23c2cbe85f5e
Carlos Damarate <damarates@hotmail.com>
2018-03-18 23:57:18 -0400

    Initializer do Banco de Dados

--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebApplication.Repository;
 
 namespace WebApplication
 {
@@ -13,6 +15,7 @@ namespace WebApplication
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Database.SetInitializer(new CreateDatabaseIfNotExists<LBContexto>());
         }
     }
 }
